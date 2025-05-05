cb1213fa8c0703d71b60eb567d47b46812b27737
VKN <vinogradov_kn@mail.ru>
2025-05-05 12:37:01 +0300

    move path const

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
 from ConfigParser import ConfigParser
 from UMLParser import UMLParser
+from settings import *
 
 import os
 
 if __name__ == '__main__':
-    # input
-    input_dir = './input'
-    cfg_json = 'config.json'
-    patched_cfg_json = 'patched_config.json'
-    imp_test_xml = 'impulse_test_input.xml'
-    # output
-    output_dir = './out/'
-    meta_json = 'meta.json'
-    cfg_xml = 'config.xml'
-    delta_json = 'delta.json'
-    res_patched_cfg_json = 'res_patched_config.json'
 
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
 
-    xml_model = UMLParser(os.path.join(input_dir, imp_test_xml))
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    xml_model = UMLParser(os.path.join(INPUT_DIR, IMP_TEST_XML))
 
     raw_data = xml_model.to_dict()
     meta_json_data = xml_model.dict_to_meta_json_format(raw_data)
     cfg_xml_data = xml_model.struct_to_config_xml()
 
-    xml_model.save_json(meta_json_data, os.path.join(output_dir, meta_json))
-    xml_model.save_xml(cfg_xml_data, os.path.join(output_dir, cfg_xml))
+    xml_model.save_json(meta_json_data, os.path.join(OUTPUT_DIR, META_JSON))
+    xml_model.save_xml(cfg_xml_data, os.path.join(OUTPUT_DIR, CFG_XML))
 
-    cfg_parser = ConfigParser(os.path.join(input_dir, cfg_json))
+    cfg_parser = ConfigParser(os.path.join(INPUT_DIR, CFG_JSON))
 
-    new_cfg_struct = cfg_parser.read_json(os.path.join(input_dir, patched_cfg_json))
+    new_cfg_struct = cfg_parser.read_json(os.path.join(INPUT_DIR, PATCHED_CFG_JSON))
     diff_struct = cfg_parser.compare(new_cfg_struct)
     new_cfg_struct_by_diff = cfg_parser.delta_applying(diff_struct)
 
-    cfg_parser.save_json(diff_struct, os.path.join(output_dir, delta_json))
-    cfg_parser.save_json(new_cfg_struct_by_diff, os.path.join(output_dir, res_patched_cfg_json))
+    cfg_parser.save_json(diff_struct, os.path.join(OUTPUT_DIR, DELTA_JSON))
+    cfg_parser.save_json(new_cfg_struct_by_diff, os.path.join(OUTPUT_DIR, RES_PATCHED_CFG_JSON))
--- settings.py
+++ settings.py
@@ -1,3 +1,17 @@
 
-UMLMODEL_STRUCTURE = ['Class','Aggregation']
 COLLECTION_TYPES = (list, tuple)
+
+# input dirs
+
+INPUT_DIR = './input'
+CFG_JSON = 'config.json'
+PATCHED_CFG_JSON = 'patched_config.json'
+IMP_TEST_XML = 'impulse_test_input.xml'
+
+# output dirs
+
+OUTPUT_DIR = './out/'
+META_JSON = 'meta.json'
+CFG_XML = 'config.xml'
+DELTA_JSON = 'delta.json'
+RES_PATCHED_CFG_JSON = 'res_patched_config.json'
